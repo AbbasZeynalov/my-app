8fcf949b191352e7f3b4ce73a88d428d5a1d8c85
vuqar <vuqarquliyev133@gmail.com>
2020-04-29 14:01:18 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container  } from "@material-ui/core";
+
+function App() {
+
+    return (
+        <Container maxWidth="md">
+            <h1>Hello world!</h1>
+        </Container >
+    );
+}
+
+export default App;
